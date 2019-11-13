bd4d4e7608cee75a69e0bfb7e542edf360e53993
Hansraj Das <raj.das.136@gmail.com>
2019-11-14 01:57:07 +0530

    Accept host ip and port from command line arguments

--- udp_client.c
+++ udp_client.c
@@ -2,23 +2,31 @@
 
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 
-int main() {
+int main(int argc, char** argv) {
   int clientSocket, portNum, nBytes;
   char buffer[1024];
+  char *host_ip = "127.0.0.1";
+  unsigned short server_port = 80;
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
+
+  if (argc > 1)
+      host_ip = argv[1];
+  if (argc > 2)
+      server_port = strtoul(argv[2], NULL, 10);
 
   /*Create UDP socket*/
   clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
   /*Configure settings in address struct*/
   serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(7891);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serverAddr.sin_port = htons(server_port);
+  serverAddr.sin_addr.s_addr = inet_addr(host_ip);
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
   /*Initialize size variable to be used later on*/
