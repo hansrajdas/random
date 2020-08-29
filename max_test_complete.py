7190cbb2e7edf3c53d0ac20eee5df19a069c518b
Hansraj Das <raj.das.136@gmail.com>
2020-08-29 18:06:39 +0530

    Ignore test which can't be completed

--- max_test_complete.py
+++ max_test_complete.py
@@ -1,17 +1,17 @@
-def solve(A):
-    _m = A[0]
-    max_idx = 0
+def max_test_complete(A):
+    _m = -1
+    max_idx = -1
     for i in range(len(A)):
-        if _m < A[i]:
+        if _m < A[i] and A[i] < len(A):
             max_idx = i
             _m = A[i]
     return ((max_idx + 1) % len(A)) + 1
 
 def main():
     n = input()
-    nums = input()
-    nums = nums.split()
-    nums = [int(n) for n in nums]
-    print(solve(nums))
+    extra_time = input()
+    extra_time = extra_time.split()
+    extra_time = [int(n) for n in extra_time]
+    print(max_test_complete(extra_time))
 
 main()
