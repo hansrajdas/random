cfc28c0775d3cde19c8f5fc2de01a2394766e51b
Hansraj Das <raj.das.136@gmail.com>
2020-10-08 01:22:16 +0530

    Use empty dict as memo

--- practice/max_product_cutting.py
+++ practice/max_product_cutting.py
@@ -12,10 +12,10 @@ def getMaxProduct(n, d):
     return _max
 
 # Main
-print(getMaxProduct(2, {'k': 1}))  # 1 -> 1*1
-print(getMaxProduct(3, {'k': 1}))  # 2 -> 1*2
-print(getMaxProduct(4, {'k': 1}))  # 4 -> 2*2
-print(getMaxProduct(5, {'k': 1}))  # 6 -> 2*3
-print(getMaxProduct(9, {'k': 1}))  # 9 -> 3*3*3
-print(getMaxProduct(10, {'k': 1}))  # 36 -> 3*3*4
-print(getMaxProduct(50, {'k': 1}))  # 86093442
+print(getMaxProduct(2, {}))  # 1 -> 1*1
+print(getMaxProduct(3, {}))  # 2 -> 1*2
+print(getMaxProduct(4, {}))  # 4 -> 2*2
+print(getMaxProduct(5, {}))  # 6 -> 2*3
+print(getMaxProduct(9, {}))  # 9 -> 3*3*3
+print(getMaxProduct(10, {}))  # 36 -> 3*3*4
+print(getMaxProduct(50, {}))  # 86093442
