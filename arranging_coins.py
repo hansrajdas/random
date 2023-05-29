53bfb81e670b69b316dba11ecdf4023a0697acd5
Hansraj Das <hansrajd@vmware.com>
2023-05-29 14:04:28 +0530

    python3

--- arranging_coins.py
+++ arranging_coins.py
@@ -2,10 +2,10 @@
 def arrangeCoins(coins):
  for c in coins:
    last = 1
-   for i in xrange(c, 1, -1):
+   for i in range(c, 1, -1):
      if 2*c < i*i + i:
        last = i
-   print last - 1
+   print(last - 1)
 
 
 arrangeCoins([2, 5, 8, 3])
